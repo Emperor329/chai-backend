4d7a5359962c5cfbd70b6e3e4ae769a5d50edaa4
Emperor329 <95495259+Emperor329@users.noreply.github.com>
2023-11-19 18:07:39 +0530

    connect to momgoatlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+/*import express from 'express'
+const app = express()
+// add a semilcolon befor IIFE
+;(async () => {
+    try {
+        await mongoose.connect(`${procees.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.error("ERROR: ", error)
+        throw error
+        }) 
+
+        app.listen(process.env.PORT , () => {
+            console.log(`App is listening on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+*/
